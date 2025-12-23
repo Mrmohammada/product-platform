368ed0b4ef63499c97383fcc93cbbbf941168fdb
Mrmohammada <mohammad.a2890@gmail.com>
2025-12-23 23:01:32 +0330

    Delete product-platform-full directory

--- product-platform-full/backend/formatter.py
+++
@@ -1,5 +0,0 @@
-def toman(price):
-    try:
-        return f"{int(float(price)):,}".replace(",", "٬") + " تومان"
-    except:
-        return "نامشخص"
--- product-platform-full/backend/logger.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-def log(msg):
-    with open("data/logs.txt","a",encoding="utf-8") as f:
-        f.write(f"{datetime.now()} - {msg}\n")
